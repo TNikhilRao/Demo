29290a9e5b6d45d9adc5c2ce2d7649fe69d59eb1
Nikhil Tirmanwar <Nikhil.Tirmanwar@synechron.com>
2018-07-27 10:51:10 +0530

    Initial Commit

---
+++ src/main/java/com/demo/jenkins/DemoForJenkins/DemoForJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.demo.jenkins.DemoForJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForJenkinsApplication.class, args);
+	}
+}
